372ee0c8506b743ab4a333fe63d7ded8e1b04a1a
John Smith <you@inf.unideb.hu>
2024-11-05 10:42:21 +0100

    JavaFX runs

---
+++ src/main/java/hu/unideb/inf/SpringJavaFX/frontend/JavaFXMain.java
@@ -0,0 +1,11 @@
+package hu.unideb.inf.SpringJavaFX.frontend;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class JavaFXMain extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println("Hello FX");
+    }
+}

9e3fa9a8a0f4a2ccd69132ba60b49298c5a93853
John Smith <you@inf.unideb.hu>
2024-11-05 10:49:24 +0100

    JavaFX window shown

--- src/main/java/hu/unideb/inf/SpringJavaFX/frontend/JavaFXMain.java
+++ src/main/java/hu/unideb/inf/SpringJavaFX/frontend/JavaFXMain.java
@@ -1,11 +1,16 @@
 package hu.unideb.inf.SpringJavaFX.frontend;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class JavaFXMain extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        System.out.println("Hello FX");
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainWindowFXML.fxml"));
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 }
